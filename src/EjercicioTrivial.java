d5362c4d09f92547d5a02bc0a29bbe2baa0bbd45
Adrián <adrialhama93@gmail.com>
2020-11-02 14:15:54 +0100

    Segunda Pregunta

--- src/EjercicioTrivial.java
+++ src/EjercicioTrivial.java
@@ -25,6 +25,7 @@ public class EjercicioTrivial {
 
 		// Se mostrará en pantalla la pregunta y la opciones se elegiran con un switch
 
+		// Primera Pregunta
 		System.out.println("1º ¿Cuál es la parte más profunda de la corteza terrestre?" + "\n1. La Antártida"
 				+ "\n2. La fosa de las Marianas" + "\n3. Himalaya" + "\n4. El Lago Ness ");
 
@@ -59,7 +60,40 @@ public class EjercicioTrivial {
 		default:
 			System.out.println("La opción introducida no es correcta, por favor inserte un número entre 1 - 4");
 		}
+
+		// Segunda Pregunta
+		System.out.println("2º ¿Cuál es la capital de Marruecos?" + "\n1. Mauritania"
+				+ "\n2. Ceuta" + "\n3. Barcelona" + "\n4. Rabat");
+
+		num = sc.nextInt();
+
+		switch (num) {
 		
+		case 1:
+			System.out.println("La respuesta es Incorrecta");
+			total -= puntos;
+			fallo++;
+			break;
+		case 2:
+			System.out.println("La respuesta es Incorrecta");
+			total -= puntos;
+			fallo++;
+			break;
+		case 3:
+			System.out.println("La respuesta es Incorrecta");
+			total -= puntos;
+			fallo++;
+			break;
+		case 4:
+			
+			System.out.println("La respuesta es Correcta");
+			total += puntos;
+			acierto++;
+			break;
+
+		default:
+			System.out.println("La opción introducida no es correcta, por favor inserte un número entre 1 - 4");
+		}
 
 		// Mensaje final total de puntos
 		System.out.println("El total de puntos es: " + total + ". El número de respuestas acertadas: " + acierto
